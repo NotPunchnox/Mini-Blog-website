4512419abc7d99a2c114f7d653b0a68f2318a890
NotPunchnox <punchnoxpaypal@gmail.com>
2024-09-22 16:17:14 +0200

    Add route for get image with name

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const upload = multer({ dest: "./imgs/" });
+const upload = multer({
+    dest: "./imgs/"
+});
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -23,14 +25,30 @@ app.get("/blogs", (req, res) => {
 app.get("/users", (req, res) => {
     return res.status(200).json(users);
 })
+app.get("/image/:id", (req, res) => {
+    if (!req.params.id) return res.status(400).json({
+        message: "bad request"
+    });
+    if (!fs.existsSync(path.resolve('./DB/images/' + req.params.id))) return res.status(404).json({
+        message: "not found!"
+    });
+
+    return res.status(200).sendFile(path.resolve('./DB/images/' + req.params.id));
+})
 app.post("/register", (req, res) => {
-    if(!req.body.username) return res.status(400).json({message: "bad request"})
+    if (!req.body.username) return res.status(400).json({
+        message: "bad request"
+    })
 })
 app.post("/login", (req, res) => {
-    if(!req.body.username) return res.status(400).json({message: "bad request"})
+    if (!req.body.username) return res.status(400).json({
+        message: "bad request"
+    })
 
     let user = users.find(a => a.name === req.body.username);
-    if(!user) return res.status(401).json({message: "unauthorized"});
+    if (!user) return res.status(401).json({
+        message: "unauthorized"
+    });
 
     return res.status(200).json(user)
 })
@@ -38,14 +56,19 @@ app.post("/login", (req, res) => {
 app.post("/upload", upload.array("images"), (req, res) => {
     var images = [];
 
-    const { name, description, content, author } = req.body;
+    const {
+        name,
+        description,
+        content,
+        author
+    } = req.body;
 
     if (req.files && req.files.length > 0) {
         req.files.forEach(file => {
             const fileName = `${Date.now()}-${file.originalname}`;
             const destPath = path.join("./DB/images/", fileName);
             fs.copyFileSync(file.path, destPath);
-            if(file.originalname.includes(".")) images.push(fileName);
+            if (file.originalname.includes(".")) images.push(fileName);
             console.log(images)
             fs.rmSync(file.path);
         });
@@ -61,7 +84,9 @@ app.post("/upload", upload.array("images"), (req, res) => {
 
     fs.writeFileSync(blogsFilePath, JSON.stringify(Blogs, null, 2), "utf8");
 
-    res.json({ images });
+    res.json({
+        images
+    });
 });
 
 const PORT = process.env.PORT || 8080;
