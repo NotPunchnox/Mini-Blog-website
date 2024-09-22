b6642087028ba9cea20042c11d857345d78184e7
NotPunchnox <punchnoxpaypal@gmail.com>
2024-09-22 17:22:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-export default 
-  {
+import {
+    defineConfig
+} from 'vite';
+
+export default defineConfig({
     build: {
-      target: "es2022"
+        target: "es2022",
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                blog: './pages/blog.html',
+                create: './pages/create.html',
+            }
+        }
     },
     esbuild: {
-      target: "es2022"
+        target: "es2022"
     },
-    optimizeDeps:{
-      esbuildOptions: {
-        target: "es2022",
-      }
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "es2022",
+        }
     }
-  }
+});
